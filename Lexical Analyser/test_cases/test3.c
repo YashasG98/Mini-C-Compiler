74ce7e70ec07bc479f3a21950cf30f9e2fa5d589
Shashwatha-Mitra <shashwathamitra@gmail.com>
2020-01-21 23:32:28 +0530

    Test cases added

---
+++ Lexical Analyser/test_cases/test3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* This program is to test the Lex Errors
+    /* This is a nested Comment */
+*/
+
+int main(){ 
+    return 0;
+}
--- run.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-lex $1
-cc -Wall -c lex.yy.c
-cc -Wall driver_code.c lex.yy.o
-./a.out < test2.c
-rm lex.yy.o lex.yy.c a.out
